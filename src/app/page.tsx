c6425079d30c1533d92283ebc8ec68dae1c6c6a1
Benjamin ⠀ <benlian23@gmail.com>
2024-06-26 10:33:20 +0800

    fixing bug where overlay does not reach button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col justify-between items-center bg-fixed bg-cover h-screen bg-no-repeat bg-center bg-[url('/images/wagons1c2.jpg')]">
-        <div className="absolute inset-0 w-full h-full bg-black bg-opacity-50"></div>
+        <div className="absolute inset-0 w-full h-screen bg-black bg-opacity-50"></div>
         <h1 className="pt-3 mt-auto text-center md:text-6xl text-4xl sm:text-5xl select-none z-10">Cars. Conversation. Culture.</h1>
         <div className="mt-auto pb-3 z-10 w-full flex flex-col justify-center items-center">
           <button onClick={()=>router.push('/editorial')} type="button" className="w-3/4 xl:w-1/2 text-white border border-white hover:border-yellow-500 hover:bg-yellow-500 focus:outline-dotted outline-offset-2 outline-1 outline-yellow-500 font-medium rounded-full text-sm px-5 py-2.5 text-center ">ENTER</button>
